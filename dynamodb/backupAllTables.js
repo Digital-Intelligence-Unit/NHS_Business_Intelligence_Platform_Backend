31ddb1493dd05e435537c99f478fbb7072b5439b
alexseymourdev <alexseymourdev@gmail.com>
2022-04-14 16:48:18 +0100

    adding new function to backup all dynamodb tables

--- dynamodb/backupAllTables.js
+++ dynamodb/backupAllTables.js
@@ -1,21 +1,70 @@
 const AWS = require("../config/dynamodb").AWS;
 var dynamodb = new AWS.DynamoDB();
+const { Parser } = require("json2csv");
+const fs = require("fs");
 
 const vpc = "dev"; // "prod";
 
-dynamodb.listTables({}, function (err, data) {
-  if (err) {
-    console.log(err, err.stack);
-  } else {
-    if (data && data.length) {
-      data.TableNames.forEach((tableName) => {
-        if (tableName.indexOf(vpc) > -1) {
-          console.log(`Backing up table: ${tableName}`);
-          backupTable(tableName);
-        }
-      });
-    } else {
-      console.log("Error: No tables found");
-    }
-  }
+// get all tables
+getAllTables(function (tables) {
+  // loop through all tables
+  tables.forEach(async (tableName) => {
+    let items = await getAllRecords(tableName);
+    // prepare the data for spreadsheet
+    let csvData = prepareSpreadsheetData(items);
+    // save the data to csv
+    const json2csvParser = new Parser();
+    const csv = json2csvParser.parse(csvData);
+    let filePath = `./dynamodb/backup_data/${tableName}.csv`;
+    fs.writeFile(filePath, csv, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("Successfully written to " + filePath);
+    });
+  });
 });
+
+function getAllTables(callback) {
+  dynamodb.listTables({}, function (err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      if (data && data.TableNames && data.TableNames.length) {
+        callback(data.TableNames);
+      } else {
+        console.log("Error: No tables found");
+      }
+    }
+  });
+}
+
+async function getAllRecords(tableName) {
+  let params = {
+    TableName: tableName,
+  };
+  let items = [];
+  let data = await dynamodb.scan(params).promise();
+  items = [...items, ...data.Items];
+  while (typeof data.LastEvaluatedKey != "undefined") {
+    params.ExclusiveStartKey = data.LastEvaluatedKey;
+    data = await dynamodb.scan(params).promise();
+    items = [...items, ...data.Items];
+  }
+  return items;
+}
+
+function prepareSpreadsheetData(data) {
+  let csvData = [];
+  data.forEach((item) => {
+    let row = {};
+    Object.keys(item).forEach((key) => {
+      Object.keys(item[key]).forEach((itemKey) => {
+        row[key] = item[key][itemKey];
+      });
+    });
+    csvData.push(row);
+  });
+  return csvData;
+}
